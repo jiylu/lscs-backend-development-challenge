84a90363e8e284fdbd303225c026b9d18c26cf4d
jilyu <jeremy_leano@dlsu.edu.ph>
2026-09-15 02:20:33 +0800

    chore: setup jest

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+export default {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+    '^utils/(.*)\\.js$': '<rootDir>/src/utils/$1',
+    '^config/(.*)\\.js$': '<rootDir>/src/config/$1',
+    '^products/(.*)\\.js$': '<rootDir>/src/products/$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+};
